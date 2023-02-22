b8cd73cef2fe799ca4792767a516276bfafaf1c8
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2023-02-22 10:19:52 -0300

    preparando repositorio

---
+++ src/main/java/com/jonathan/springframeworkproject_1/SpringFrameworkProject1Application.java
@@ -0,0 +1,13 @@
+package com.jonathan.springframeworkproject_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkProject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonathan/springframeworkproject_1/SpringFrameworkProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.springframeworkproject_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
